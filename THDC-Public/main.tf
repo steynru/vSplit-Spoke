79465832fc5ce8b6a8db8720f6007549060ccc19
Ruan Steyn <59763740+steynru@users.noreply.github.com>
2022-07-20 10:36:50 +0200

    Changed Networking

--- THDC-Public/main.tf
+++ THDC-Public/main.tf
@@ -1,7 +1,3 @@
-#variable "azure_sub_id_vscode_local" {
-#  type = string
-#  default = ""
-#}
 
 variable "client_access_secret_id" {
   type = string
@@ -18,44 +14,54 @@ variable "azure_directory_id" {
   default = ""
 }
 
-#variable "long_name" {
-#  type = string
-#  default = ""
-#}
-
-#variable "short_name" {
-#  type = string
-#  default = ""
-#}
-
-#variable "resource_location" {
-#  type = string
-#  default = ""
-#}
-
-
 ########### Management 1  ###########
 resource "azurerm_resource_group" "Spoke-Infra-RG" {
   name     = "${var.short_name}-INFRA-RG"
   location = var.resource_location
 }
 
-resource "azurerm_virtual_network" "Spoke-Infra-VNET" {
-  name                = "${var.short_name}-VNET"
-  location            = azurerm_resource_group.Spoke-Infra-RG.location
-  resource_group_name = azurerm_resource_group.Spoke-Infra-RG.name
-  address_space       = ["10.10.0.0/16"]
+resource "azurerm_resource_group" "Spoke-App-RG" {
+  name     = "${var.short_name}-APP-RG"
+  location = var.resource_location
 }
 
-resource "azurerm_subnet" "subnet_1" {
-  name                 = "${var.short_name}-SUBNET1"
+resource "azurerm_virtual_network" "Spoke-Infra-VNET" {
+  name                = "${var.short_name}-Spoke-VNET"
+  location            = azurerm_resource_group.Spoke-Infra-RG.location
+  resource_group_name = azurerm_resource_group.Spoke-Infra-RG.name
+  address_space       = var.vnet_ip_range
+}
+
+resource "azurerm_subnet" "Infra-Subnet" {
+  name                 = "${var.short_name}-infra"
   virtual_network_name = azurerm_virtual_network.Spoke-Infra-VNET.name
   resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
-  address_prefixes     = ["10.10.10.0/24"]
+  address_prefixes     = var.infra-subnet
+}
+
+resource "azurerm_subnet" "DB-Subnet" {
+  name                 = "${var.short_name}-db"
+  virtual_network_name = azurerm_virtual_network.Spoke-Infra-VNET.name
+  resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
+  address_prefixes     = var.db-subnet
+}
+
+resource "azurerm_subnet" "App-Subnet" {
+  name                 = "${var.short_name}-app"
+  virtual_network_name = azurerm_virtual_network.Spoke-Infra-VNET.name
+  resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
+  address_prefixes     = var.app-subnet
+}
+
+resource "azurerm_subnet" "Web-Subnet" {
+  name                 = "${var.short_name}-web"
+  virtual_network_name = azurerm_virtual_network.Spoke-Infra-VNET.name
+  resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
+  address_prefixes     = var.web-subnet
 }
 
 resource "azurerm_virtual_network_peering" "example-1" {
-  name                      = "HubToSpoke"
+  name                      = "SpokeToHub"
   resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
   virtual_network_name      = azurerm_virtual_network.Spoke-Infra-VNET.name
   remote_virtual_network_id = "/subscriptions/a008b1c5-30b6-4611-b9da-d01e0e529f51/resourceGroups/VSneuHub-INFRA-RG/providers/Microsoft.Network/virtualNetworks/VSneuHub-VNET"
