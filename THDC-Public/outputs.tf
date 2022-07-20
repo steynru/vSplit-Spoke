3d23dae6db579574ef8710308c038340476aea9d
Ruan Steyn <59763740+steynru@users.noreply.github.com>
2022-07-20 10:44:00 +0200

    Update outputs.tf

--- THDC-Public/outputs.tf
+++ THDC-Public/outputs.tf
@@ -1,11 +1,23 @@
-output "Spoke-Infra-RG" {
-  value = azurerm_resource_group.Spoke-Infra-RG.name
+output "Spoke-APP-RG" {
+  value = azurerm_resource_group.Spoke-APP-RG.name
 }
 
 output "Spoke-Infra-VNET" {
   value = azurerm_virtual_network.Spoke-Infra-VNET
 }
 
-output "subnet_1" {
-  value = azurerm_subnet.subnet_1
+output "Infra-Subnet" {
+  value = azurerm_subnet.Infra-Subnet
+}
+
+output "DB-Subnet" {
+  value = azurerm_subnet.DB-Subnet
+}
+
+output "App-Subnet" {
+  value = azurerm_subnet.App-Subnet
+}
+
+output "Web-Subnet" {
+  value = azurerm_subnet.Web-Subnet
 }
