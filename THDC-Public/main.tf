07cd409f1f7c6e69a4da406d26734b866859fdbb
Ruan Steyn <59763740+steynru@users.noreply.github.com>
2022-07-20 11:10:14 +0200

    Update main.tf

--- THDC-Public/main.tf
+++ THDC-Public/main.tf
@@ -60,9 +60,10 @@ resource "azurerm_subnet" "Web-Subnet" {
   address_prefixes     = var.web-subnet
 }
 
-resource "azurerm_virtual_network_peering" "example-1" {
-  name                      = "SpokeToHub"
-  resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
-  virtual_network_name      = azurerm_virtual_network.Spoke-Infra-VNET.name
-  remote_virtual_network_id = data.tfe_outputs.Hub.Hub-Infra-VNET.id#"/subscriptions/a008b1c5-30b6-4611-b9da-d01e0e529f51/resourceGroups/VSneuHub-INFRA-RG/providers/Microsoft.Network/virtualNetworks/VSneuHub-VNET"
-}
+#resource "azurerm_virtual_network_peering" "example-1" {
+#  name                      = "SpokeToHub"
+#  resource_group_name  = azurerm_resource_group.Spoke-Infra-RG.name
+#  virtual_network_name      = azurerm_virtual_network.Spoke-Infra-VNET.name
+#  remote_virtual_network_id = data.tfe_outputs.Hub.Hub-Infra-VNET.id
+##  "/subscriptions/a008b1c5-30b6-4611-b9da-d01e0e529f51/resourceGroups/VSneuHub-INFRA-RG/providers/Microsoft.Network/virtualNetworks/VSneuHub-VNET"
+#}
